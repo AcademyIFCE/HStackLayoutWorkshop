f9350f7b16f0844e4db4ea8903f30403cc459012
Mateus Rodrigues <mateus.fr26@gmail.com>
2022-10-19 10:07:55 -0300

    Add base implementation to `MyHStackLayout`

--- HStackLayoutWorkshop/MyHStackLayout.swift
+++ HStackLayoutWorkshop/MyHStackLayout.swift
@@ -11,11 +11,41 @@ struct MyHStackLayout: Layout {
     }
     
     func sizeThatFits(proposal: ProposedViewSize, subviews: Subviews, cache: inout Cache) -> CGSize {
-        fatalError("\(#function) not implemented")
+        
+        let proposedHeight = proposal.replacingUnspecifiedDimensions().height
+        let proposedWidth = proposal.replacingUnspecifiedDimensions().width/CGFloat(subviews.count)
+        
+        let proposedSize = ProposedViewSize(width: proposedWidth, height: proposedHeight)
+        
+        var sizes: [CGSize] = []
+        
+        for subview in subviews {
+            let size = subview.sizeThatFits(proposedSize)
+            sizes.append(size)
+        }
+        
+        let widthThatFits = sizes.reduce(0) { $0 + $1.width }
+        let heightThatFits = sizes.reduce(0) { max($0, $1.height) }
+        
+        return CGSize(width: widthThatFits, height: heightThatFits)
+        
     }
     
     func placeSubviews(in bounds: CGRect, proposal: ProposedViewSize, subviews: Subviews, cache: inout Cache) {
-        fatalError("\(#function) not implemented")
+        
+        var position = CGPoint(x: bounds.minX, y: bounds.midY)
+        
+        let proposedHeight = proposal.replacingUnspecifiedDimensions().height
+        let proposedWidth = proposal.replacingUnspecifiedDimensions().width/CGFloat(subviews.count)
+        
+        let proposedSize = ProposedViewSize(width: proposedWidth, height: proposedHeight)
+        
+        for subview in subviews {
+            let size = subview.sizeThatFits(proposedSize)
+            subview.place(at: position, anchor: .leading, proposal: proposedSize)
+            position.x += size.width
+        }
+        
     }
     
 }
